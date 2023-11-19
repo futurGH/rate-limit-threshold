3e85fc41b3b522f32b6c23521569b49fdf3e6dc5
Borewit <Borewit@users.noreply.github.com>
2023-11-19 22:26:29 +0100

    Remove dependency on debug

--- lib/rate-limit-threshold.ts
+++ lib/rate-limit-threshold.ts
@@ -1,7 +1,3 @@
-import Debug from 'debug';
-
-const debug = Debug('musicbrainz-api:rate-limiter');
-
 export class RateLimitThreshold {
 
   public static sleep(ms: number): Promise<void> {
@@ -16,7 +12,6 @@ export class RateLimitThreshold {
    * @param period Period in milliseconds
    */
   public constructor(private requests: number, period: number) {
-    debug(`Rate limiter initialized with max ${requests} calls in ${period} seconds.`);
     this.period = 1000 * period;
   }
 
@@ -34,7 +29,6 @@ export class RateLimitThreshold {
     // debug(`Current rate is  ${this.queue.length} per ${this.period / 1000} sec`);
     if (this.queue.length >= this.requests) {
       delay = this.queue[0] + this.period - now;
-      debug(`Client side rate limiter activated: cool down for ${delay / 1000} s...`);
       await RateLimitThreshold.sleep(delay);
     }
     this.queue.push(new Date().getTime());
